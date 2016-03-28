e9493566159efbcfae0ede2610bb7de1750896e9
Grigor Ambartsumyan <ambartsumyan.grigor@gmail.com>
2016-03-28 06:18:51 -0700

    Finished Part III
    There was a problem in qsys with the RAM clock, and several problems in
    the verilog file that caused it to only read half of the picture, and
    stop output based on old write address

---
+++ Part_III_HW/lab4/lab4_bb.v
@@ -0,0 +1,140 @@
+
+module lab4 (
+	clk_65_clk,
+	clocked_video_export_vid_clk,
+	clocked_video_export_vid_data,
+	clocked_video_export_underflow,
+	clocked_video_export_vid_datavalid,
+	clocked_video_export_vid_v_sync,
+	clocked_video_export_vid_h_sync,
+	clocked_video_export_vid_f,
+	clocked_video_export_vid_h,
+	clocked_video_export_vid_v,
+	cont_from_hps_export,
+	done_to_hps_export,
+	hps_0_h2f_reset_reset_n,
+	hps_io_hps_io_sdio_inst_CMD,
+	hps_io_hps_io_sdio_inst_D0,
+	hps_io_hps_io_sdio_inst_D1,
+	hps_io_hps_io_sdio_inst_CLK,
+	hps_io_hps_io_sdio_inst_D2,
+	hps_io_hps_io_sdio_inst_D3,
+	hps_io_hps_io_usb1_inst_D0,
+	hps_io_hps_io_usb1_inst_D1,
+	hps_io_hps_io_usb1_inst_D2,
+	hps_io_hps_io_usb1_inst_D3,
+	hps_io_hps_io_usb1_inst_D4,
+	hps_io_hps_io_usb1_inst_D5,
+	hps_io_hps_io_usb1_inst_D6,
+	hps_io_hps_io_usb1_inst_D7,
+	hps_io_hps_io_usb1_inst_CLK,
+	hps_io_hps_io_usb1_inst_STP,
+	hps_io_hps_io_usb1_inst_DIR,
+	hps_io_hps_io_usb1_inst_NXT,
+	hps_io_hps_io_uart0_inst_RX,
+	hps_io_hps_io_uart0_inst_TX,
+	led_export_export,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	ready_from_hps_export,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n,
+	sobel_0_debug_cont,
+	sobel_0_debug_state,
+	sobel_0_signals_done,
+	sobel_0_signals_ready,
+	sw_export_export,
+	sdram_clk_clk,
+	system_ref_clk_clk,
+	system_ref_reset_reset);	
+
+	output		clk_65_clk;
+	input		clocked_video_export_vid_clk;
+	output	[31:0]	clocked_video_export_vid_data;
+	output		clocked_video_export_underflow;
+	output		clocked_video_export_vid_datavalid;
+	output		clocked_video_export_vid_v_sync;
+	output		clocked_video_export_vid_h_sync;
+	output		clocked_video_export_vid_f;
+	output		clocked_video_export_vid_h;
+	output		clocked_video_export_vid_v;
+	output		cont_from_hps_export;
+	input		done_to_hps_export;
+	output		hps_0_h2f_reset_reset_n;
+	inout		hps_io_hps_io_sdio_inst_CMD;
+	inout		hps_io_hps_io_sdio_inst_D0;
+	inout		hps_io_hps_io_sdio_inst_D1;
+	output		hps_io_hps_io_sdio_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_D2;
+	inout		hps_io_hps_io_sdio_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D0;
+	inout		hps_io_hps_io_usb1_inst_D1;
+	inout		hps_io_hps_io_usb1_inst_D2;
+	inout		hps_io_hps_io_usb1_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D4;
+	inout		hps_io_hps_io_usb1_inst_D5;
+	inout		hps_io_hps_io_usb1_inst_D6;
+	inout		hps_io_hps_io_usb1_inst_D7;
+	input		hps_io_hps_io_usb1_inst_CLK;
+	output		hps_io_hps_io_usb1_inst_STP;
+	input		hps_io_hps_io_usb1_inst_DIR;
+	input		hps_io_hps_io_usb1_inst_NXT;
+	input		hps_io_hps_io_uart0_inst_RX;
+	output		hps_io_hps_io_uart0_inst_TX;
+	output	[9:0]	led_export_export;
+	output	[14:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[31:0]	memory_mem_dq;
+	inout	[3:0]	memory_mem_dqs;
+	inout	[3:0]	memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output	[3:0]	memory_mem_dm;
+	input		memory_oct_rzqin;
+	output		ready_from_hps_export;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output		sdram_wire_cs_n;
+	inout	[15:0]	sdram_wire_dq;
+	output	[1:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+	input		sobel_0_debug_cont;
+	output	[2:0]	sobel_0_debug_state;
+	output		sobel_0_signals_done;
+	input		sobel_0_signals_ready;
+	input	[9:0]	sw_export_export;
+	output		sdram_clk_clk;
+	input		system_ref_clk_clk;
+	input		system_ref_reset_reset;
+endmodule
